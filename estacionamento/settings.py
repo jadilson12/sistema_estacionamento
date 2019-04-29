fb7b8d2ef158af67367510ede40eb60eb6b31dbd
Jadilson Guedes <jadilson12@gmail.com>
2019-04-29 17:31:21 -0300

    settings: adicionado permissão de acesso para Host

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k=1km*hfjh8a4ni-l3hddq=46r6m(&(iqpjtj@(-nvf$z(5fx@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
