46637423c07218bfc5774af3972949c161217399
Jadilson Guedes <jadilson12@gmail.com>
2019-05-01 09:01:59 -0300

    urls: adiciona  listas  para as identidades

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from core import views as v
 
 urlpatterns = [
-    path('', home, name='core_home')
+    path('', v.home, name='core_home'),
+    path('pessoas/', v.lista_pessoas, name='core_lista_pessoas'),
+    path('veiculos/', v.lista_veiculos, name='core_lista_veiculos'),
+    path('mov-rot/', v.lista_movrotativo, name='core_movrotativo'),
+    path('mensalista/', v.lista_mensalista, name='core_mensalista'),
+    path('mov-mens/', v.lista_movmensalista, name='core_movmensalista')
 ]
