261365125106a24107938294c472178bb5f27129
Jadilson Guedes <jadilson12@gmail.com>
2019-05-01 10:04:31 -0300

    views: adiciona o create para com identidades

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import (
     Pessoa,
     Veiculo,
     MovRotativo,
     Mensalista,
     MovMensalista
+)
+from .forms import (
+    PessoaForm,
+    MovRotativoForm,
+    MovMensalistaForm,
+    MensalistaForm,
+    VeiculoForm
 )
 
 
@@ -15,27 +22,69 @@ def home(request):
 
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
-    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+    form = PessoaForm()
+    data = {'pessoas': pessoas, 'form': form}
+    return render(request, 'core/lista_pessoas.html', data)
+
+
+def criar_pessoas(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_pessoas')
 
 
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
-    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+    form = VeiculoForm()
+    data = {'veiculos': veiculos, 'form': form}
+    return render(request, 'core/lista_veiculos.html', data)
+
+
+def criar_veiculos(request):
+    form = VeiculoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_veiculos')
 
 
 def lista_movrotativo(request):
     mov_rot = MovRotativo.objects.all()
-    return render(
-        request, 'core/lista_movrotativo.html', {'movrotativo': mov_rot})
+    form = MovRotativoForm()
+    data = {'movrotativo': mov_rot, 'form' : form}
+    return render(request, 'core/lista_movrotativo.html', data)
+
+
+def criar_movrotativo(request):
+    form = MovRotativoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_movrotativo')
 
 
 def lista_mensalista(request):
     mensalistas = Mensalista.objects.all()
-    return render(
-        request, 'core/lista_mensalista.html', {'mensalistas': mensalistas})
+    form = MensalistaForm()
+    data = {'mensalistas': mensalistas, 'form': form}
+    return render(request, 'core/lista_mensalista.html', data)
+
+
+def criar_mensalista(request):
+    form = MensalistaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_mensalista')
 
 
 def lista_movmensalista(request):
     mov_mens = MovMensalista.objects.all()
-    return render(
-        request, 'core/lista_movmensalista.html', {'mov_mens': mov_mens})
+    form = MovMensalistaForm()
+    data = {'mov_mens': mov_mens, 'form': form}
+    return render(request, 'core/lista_movmensalista.html', data)
+
+
+def criar_movmensalista(request):
+    form = MovMensalistaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_movmensalista')
