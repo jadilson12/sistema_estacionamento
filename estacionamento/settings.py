bced148b1106df01481ec92ab2262025a77a0d8e
Jadilson Guedes <jadilson12@gmail.com>
2019-05-01 15:19:01 -0300

    template: adiciona uma base para o projeto

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'estacionamento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
