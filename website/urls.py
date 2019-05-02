7de221fc3511a12066f609175d55824a019bc375
Jadilson Guedes <jadilson12@gmail.com>
2019-05-02 09:27:06 -0300

    website: adiciona a pagina contato

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, contato
 
 urlpatterns = [
     path('', home, name='website_home'),
+    path('contato/', contato, name='website_contato'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(resquest):
     return render(resquest, 'website/index.html')
+
+
+def contato(resquest):
+    return render(resquest, 'website/contato.html')
